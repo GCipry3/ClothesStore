c0d11f8d0a5a1f2ee561ed4b8350cdcd380e0c80
GCipry3 <galbeazaciprian30@gmail.com>
2022-12-29 23:19:59 +0200

    added price fields into order and order_items tables

--- database_management.py
+++ database_management.py
@@ -67,6 +67,7 @@ def create_tables():
             order_id INT PRIMARY KEY AUTO_INCREMENT,
             customer_id INT NOT NULL,
             order_date DATETIME NOT NULL,
+            total_price INT NOT NULL,
             shipping_address VARCHAR(255) NOT NULL,
             FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
         )
@@ -78,6 +79,7 @@ def create_tables():
             order_id INT NOT NULL,
             product_id INT NOT NULL,
             quantity INT NOT NULL,
+            order_item_price INT NOT NULL,
             PRIMARY KEY (order_id, product_id),
             FOREIGN KEY (order_id) REFERENCES orders(order_id),
             FOREIGN KEY (product_id) REFERENCES products(product_id)
@@ -171,34 +173,34 @@ def insert_data():
 
     #orders
     cursor.execute('''
-        INSERT INTO orders (customer_id, order_date, shipping_address)
+        INSERT INTO orders (customer_id, order_date,total_price, shipping_address)
         VALUES
-            (1, '2020-01-01 00:00:00', 'Address 1'),
-            (2, '2020-01-02 00:00:00', 'Address 2'),
-            (3, '2020-01-03 00:00:00', 'Address 3'),
-            (4, '2020-01-04 00:00:00', 'Address 4'),
-            (5, '2020-01-05 00:00:00', 'Address 5'),
-            (6, '2020-01-06 00:00:00', 'Address 6'),
-            (7, '2020-01-07 00:00:00', 'Address 7'),
-            (8, '2020-01-08 00:00:00', 'Address 8'),
-            (9, '2020-01-09 00:00:00', 'Address 9'),
-            (10, '2020-01-10 00:00:00', 'Address 10')
+            (1, '2020-01-01 00:00:00',55, 'Address 1'),
+            (2, '2020-01-02 00:00:00',55, 'Address 2'),
+            (3, '2020-01-03 00:00:00',55, 'Address 3'),
+            (4, '2020-01-04 00:00:00',55, 'Address 4'),
+            (5, '2020-01-05 00:00:00',55, 'Address 5'),
+            (6, '2020-01-06 00:00:00',55, 'Address 6'),
+            (7, '2020-01-07 00:00:00',55, 'Address 7'),
+            (8, '2020-01-08 00:00:00',55, 'Address 8'),
+            (9, '2020-01-09 00:00:00',55, 'Address 9'),
+            (10, '2020-01-10 00:00:00',55, 'Address 10')
     ''')
 
     #order_items
     cursor.execute('''
-        INSERT INTO order_items (order_id, product_id, quantity)
+        INSERT INTO order_items (order_id, product_id, quantity,order_item_price)
         VALUES
-            (1, 1, 1),
-            (2, 2, 2),
-            (3, 3, 3),
-            (4, 4, 4),
-            (5, 5, 5),
-            (6, 6, 6),
-            (7, 7, 7),
-            (8, 8, 8),
-            (9, 9, 9),
-            (10, 10, 10)
+            (1, 1, 1, 10),
+            (2, 2, 2, 10),
+            (3, 3, 3, 10),
+            (4, 4, 4, 10),
+            (5, 5, 5, 10),
+            (6, 6, 6, 10),
+            (7, 7, 7, 10),
+            (8, 8, 8, 10),
+            (9, 9, 9, 10),
+            (10, 10, 10, 10)
     ''')
 
     conn.commit()
